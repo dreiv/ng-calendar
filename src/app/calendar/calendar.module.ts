308e73bc8fcf05fad1f748263f6b53ff3ba420c6
Andrei Voicu <dreivoicu@gmail.com>
2020-05-21 19:17:58 +0300

    added tracks

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
-import { CalendarBodyComponent } from './calendar-body/calendar-body.component';
 
 @NgModule({
   declarations: [
-    CalendarComponent,
-    CalendarHeaderComponent,
-    CalendarBodyComponent
+    CalendarComponent
   ],
   imports: [
     CommonModule
