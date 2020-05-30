d0f21518fb42a6b37668b5c7c2f57429d3b50fbf
Andrei Voicu <dreivoicu@gmail.com>
2020-05-30 09:29:23 +0300

    added actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalendarModule } from './calendar/calendar.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CalendarModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CalendarModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArrowLeftComponent } from './icons/arrow-left/arrow-left.component';
 import { ArrowRightComponent } from './icons/arrow-right/arrow-right.component';
+import { PlusComponent } from './icons/plus/plus.component';
 
 @NgModule({
-  declarations: [ArrowLeftComponent, ArrowRightComponent],
+  declarations: [ArrowLeftComponent, ArrowRightComponent, PlusComponent],
   imports: [CommonModule],
-  exports: [ArrowLeftComponent, ArrowRightComponent]
+  exports: [ArrowLeftComponent, ArrowRightComponent, PlusComponent]
 })
 export class SharedModule {}
