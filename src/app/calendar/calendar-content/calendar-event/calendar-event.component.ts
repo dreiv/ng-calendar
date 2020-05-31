5eac746c8c3a64b0fab2181b00b6f89d37bec3e2
Andrei Voicu <dreivoicu@gmail.com>
2020-05-31 17:08:38 +0300

    positioned event

--- src/app/calendar/calendar-content/calendar-event/calendar-event.component.ts
+++ src/app/calendar/calendar-content/calendar-event/calendar-event.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CalendarEvent } from '../../calendar';
+import {
+  Component,
+  OnInit,
+  Input,
+  HostBinding,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { CalendarEvent, HOUR_SIZE } from '../../calendar';
 
 @Component({
   selector: 'app-calendar-event',
   templateUrl: './calendar-event.component.html',
-  styleUrls: ['./calendar-event.component.scss']
+  styleUrls: ['./calendar-event.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalendarEventComponent implements OnInit {
+  @HostBinding('style.top')
+  get offsetTop(): string {
+    const date = this.event.date;
+    const time = date.getHours() + date.getMinutes() / 60;
+
+    return time * HOUR_SIZE + 'px';
+  }
   @Input() event: CalendarEvent;
   constructor() {}
 
--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -1,3 +1,6 @@
+// change together with styling
+export const HOUR_SIZE = 48;
+
 export type CalendarPeriod = 'day' | 'week' | 'workWeek';
 
 export type CalendarDirection = 'previous' | 'current' | 'next';
